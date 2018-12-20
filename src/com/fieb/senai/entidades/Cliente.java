05caf6ceda36efdd19309982c63a32e002a642bb
lenda26 <yuri.otricolor@gmail.com>
2018-12-20 11:24:52 -0300

    Classe Cliente

--- src/com/fieb/senai/entidades/Cliente.java
+++ src/com/fieb/senai/entidades/Cliente.java
@@ -5,10 +5,98 @@
  */
 package com.fieb.senai.entidades;
 
+import java.util.Objects;
+
 /**
  *
  * @author yuri_
  */
 public class Cliente {
+    
+    private String nome;
+    private Integer id;
+    private Integer idade;
+    private Sexo sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.idade);
+        hash = 67 * hash + Objects.hashCode(this.sexo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        if (!Objects.equals(this.sexo, other.sexo)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Cliente(String nome, Integer id, Integer idade, Sexo sexo) {
+        this.nome = nome;
+        this.id = id;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public Cliente() {
+    }
+    
+    
     
 }
